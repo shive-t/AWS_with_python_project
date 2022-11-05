5106e0555e909be70c20bd14515d5a052a9a2108
shive-t <108564986+shive-t@users.noreply.github.com>
2022-11-05 15:22:16 +0530

    Update qrcode_gen.py

--- .idea/qrcode_gen.py
+++ .idea/qrcode_gen.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
 import pyqrcode
+import boto3
+import random
+num=random.randint(0,1000)
+filename="code"+str(num)+".png"
 qr = input("enter link/variable to be converted to QRCode:\n")
 link = pyqrcode.create(qr)
-link.png('code.png', scale=10)
+link.png(filename, scale=10)
+s3 = boto3.resource('s3')
+response = s3.meta.client.upload_file(filename, 'qrcode-storage',filename)
